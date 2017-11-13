9e3c8dd43c617814f5e577204ade2f9da95b93dc
DilumN <dilumnavanjana@gmail.com>
2017-11-13 14:29:15 +0800

    added docker network

--- app/contract_sample.rb
+++ app/contract_sample.rb
@@ -2,7 +2,8 @@ require 'elchapo'
 require 'pry'
 
 # client = Ethereum::Connection.new("/root/.ethereum/rinkeby/geth.ipc", false)
-client = Ethereum::HttpConnection.new("http://0.0.0.0:8545")
+client = Bitcoin::HttpConnection.new("http://127.0.0.1:18333", user: "foo", pass: "bar")
+# client = Ethereum::HttpConnection.new("http://0.0.0.0:8545")
 binding.pry
 # contract = Ethereum::Contract.create(file: "greeter.sol", client: client)
 # address = contract.deploy_and_wait("Hello from ethereum.rb!")
